5e52e3a22887a95b0efd0668f7c427e127ef0eba
Surajkumar70 <name>
2022-06-18 10:05:26 +0530

    E-mail sending

---
+++ index.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+require("dotenv").config()
+let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    auth: {
+        user: process.env.user,
+        pass: process.env.password
+    }
+});
+  
+let mailDetails = {
+    from: 'sk5105938@gmail.com',
+    to: 'sajjad21@navgurukul.org',
+    cc:'khaja21@navgurukul.org',
+    bcc:'ss5105928@gmail.com',
+    subject: 'Test mail',
+    text: 'HI everyone I m  Surajkumar from up'
+};
+  
+mailTransporter.sendMail(mailDetails, function(err, data) {
+    if(err) {
+        console.log('Error Occurs');
+    } else {
+        console.log('Email sent successfully');
+    }
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server is on :${port}`)
+})
+
+
+
